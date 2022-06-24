d230d9ba771a40bc50cb339d21f5d10f046d8ffb
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-06-24 22:03:01 +0900

    Add javascript dap

--- lua/debug-configuration.lua
+++ lua/debug-configuration.lua
@@ -65,6 +65,32 @@ dap.configurations.python = {
   },
 }
 
+dap.adapters.node2 = {
+  type = 'executable',
+  command = 'node',
+  args = { os.getenv('HOME') .. '/Applications//vscode-node-debug2/out/src/nodeDebug.js' },
+}
+
+dap.configurations.javascript = {
+  {
+    name = 'Launch',
+    type = 'node2',
+    request = 'launch',
+    program = '${file}',
+    cwd = vim.fn.getcwd(),
+    sourceMaps = true,
+    protocol = 'inspector',
+    console = 'integratedTerminal',
+  },
+  {
+    -- For this to work you need to make sure the node process is started with the `--inspect` flag.
+    name = 'Attach to process',
+    type = 'node2',
+    request = 'attach',
+    processId = require 'dap.utils'.pick_process,
+  },
+}
+
 -- remember to delete vscode launch.json trailing comma
 require('dap.ext.vscode').load_launchjs()
 
@@ -118,6 +144,8 @@ require("dapui").setup({
     max_type_length = nil, -- Can be integer or nil.
   }
 })
+
+
 
 require('nvim-dap-virtual-text').setup()
 
