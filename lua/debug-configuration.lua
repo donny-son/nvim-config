4555cd3803300b33ef737cd37f0b8946e3ef4586
Dongook Son <60206749+donny-son@users.noreply.github.com>
2022-06-27 09:01:31 +0900

    Change dapui

--- lua/debug-configuration.lua
+++ lua/debug-configuration.lua
@@ -94,7 +94,7 @@ require("dapui").setup({
         'stacks',
         'watches',
       },
-      size = 40,
+      size = 60,
       position = 'left',
     },
     {
@@ -106,26 +106,6 @@ require("dapui").setup({
       position = 'bottom',
     },
   },
-  -- sidebar = {
-  --   -- You can change the order of elements in the sidebar
-  --   elements = {
-  --     -- Provide as ID strings or tables with "id" and "size" keys
-  --     {
-  --       id = "scopes",
-  --       size = 0.35, -- Can be float or integer > 1
-  --     },
-  --     { id = "breakpoints", size = 0.15 },
-  --     { id = "stacks", size = 0.25 },
-  --     { id = "watches", size = 0.25 },
-  --   },
-  --   size = 70,
-  --   position = "right", -- Can be "left", "right", "top", "bottom"
-  -- },
-  -- tray = {
-  --   elements = { "repl" },
-  --   size = 15,
-  --   position = "bottom", -- Can be "left", "right", "top", "bottom"
-  -- },
   floating = {
     max_height = nil, -- These can be integers or a float between 0 and 1.
     max_width = nil, -- Floats will be treated as percentage of your screen.
